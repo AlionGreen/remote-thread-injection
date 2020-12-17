094423d128cd43be4f35eac30d469bc3f21de2af
AlionGreen <ali.sabzeghabaei7@gmail.com>
2020-12-17 10:41:32 +0330

    Create syscalls.asm

---
+++ syscalls/syscalls.asm
@@ -0,0 +1,287 @@
+.code
+
+NtOpenProcess PROC
+	mov rax, gs:[60h]                       ; Load PEB into RAX.
+NtOpenProcess_Check_X_X_XXXX:               ; Check major version.
+	cmp dword ptr [rax+118h], 10
+	je  NtOpenProcess_Check_10_0_XXXX
+	jmp NtOpenProcess_SystemCall_Unknown
+NtOpenProcess_Check_10_0_XXXX:              ; Check build number for Windows 10.
+	cmp word ptr [rax+120h], 10240
+	je  NtOpenProcess_SystemCall_10_0_10240
+	cmp word ptr [rax+120h], 10586
+	je  NtOpenProcess_SystemCall_10_0_10586
+	cmp word ptr [rax+120h], 14393
+	je  NtOpenProcess_SystemCall_10_0_14393
+	cmp word ptr [rax+120h], 15063
+	je  NtOpenProcess_SystemCall_10_0_15063
+	cmp word ptr [rax+120h], 16299
+	je  NtOpenProcess_SystemCall_10_0_16299
+	cmp word ptr [rax+120h], 17134
+	je  NtOpenProcess_SystemCall_10_0_17134
+	cmp word ptr [rax+120h], 17763
+	je  NtOpenProcess_SystemCall_10_0_17763
+	cmp word ptr [rax+120h], 18362
+	je  NtOpenProcess_SystemCall_10_0_18362
+	cmp word ptr [rax+120h], 18363
+	je  NtOpenProcess_SystemCall_10_0_18363
+	cmp word ptr [rax+120h], 19041
+	je  NtOpenProcess_SystemCall_10_0_19041
+	cmp word ptr [rax+120h], 19042
+	je  NtOpenProcess_SystemCall_10_0_19042
+	jmp NtOpenProcess_SystemCall_Unknown
+NtOpenProcess_SystemCall_10_0_10240:        ; Windows 10.0.10240 (1507)
+	mov eax, 0026h
+	jmp NtOpenProcess_Epilogue
+NtOpenProcess_SystemCall_10_0_10586:        ; Windows 10.0.10586 (1511)
+	mov eax, 0026h
+	jmp NtOpenProcess_Epilogue
+NtOpenProcess_SystemCall_10_0_14393:        ; Windows 10.0.14393 (1607)
+	mov eax, 0026h
+	jmp NtOpenProcess_Epilogue
+NtOpenProcess_SystemCall_10_0_15063:        ; Windows 10.0.15063 (1703)
+	mov eax, 0026h
+	jmp NtOpenProcess_Epilogue
+NtOpenProcess_SystemCall_10_0_16299:        ; Windows 10.0.16299 (1709)
+	mov eax, 0026h
+	jmp NtOpenProcess_Epilogue
+NtOpenProcess_SystemCall_10_0_17134:        ; Windows 10.0.17134 (1803)
+	mov eax, 0026h
+	jmp NtOpenProcess_Epilogue
+NtOpenProcess_SystemCall_10_0_17763:        ; Windows 10.0.17763 (1809)
+	mov eax, 0026h
+	jmp NtOpenProcess_Epilogue
+NtOpenProcess_SystemCall_10_0_18362:        ; Windows 10.0.18362 (1903)
+	mov eax, 0026h
+	jmp NtOpenProcess_Epilogue
+NtOpenProcess_SystemCall_10_0_18363:        ; Windows 10.0.18363 (1909)
+	mov eax, 0026h
+	jmp NtOpenProcess_Epilogue
+NtOpenProcess_SystemCall_10_0_19041:        ; Windows 10.0.19041 (2004)
+	mov eax, 0026h
+	jmp NtOpenProcess_Epilogue
+NtOpenProcess_SystemCall_10_0_19042:        ; Windows 10.0.19042 (20H2)
+	mov eax, 0026h
+	jmp NtOpenProcess_Epilogue
+NtOpenProcess_SystemCall_Unknown:           ; Unknown/unsupported version.
+	ret
+NtOpenProcess_Epilogue:
+	mov r10, rcx
+	syscall
+	ret
+NtOpenProcess ENDP
+
+NtAllocateVirtualMemory PROC
+	mov rax, gs:[60h]                                 ; Load PEB into RAX.
+NtAllocateVirtualMemory_Check_X_X_XXXX:               ; Check major version.
+	cmp dword ptr [rax+118h], 10
+	je  NtAllocateVirtualMemory_Check_10_0_XXXX
+	jmp NtAllocateVirtualMemory_SystemCall_Unknown
+NtAllocateVirtualMemory_Check_10_0_XXXX:              ; Check build number for Windows 10.
+	cmp word ptr [rax+120h], 10240
+	je  NtAllocateVirtualMemory_SystemCall_10_0_10240
+	cmp word ptr [rax+120h], 10586
+	je  NtAllocateVirtualMemory_SystemCall_10_0_10586
+	cmp word ptr [rax+120h], 14393
+	je  NtAllocateVirtualMemory_SystemCall_10_0_14393
+	cmp word ptr [rax+120h], 15063
+	je  NtAllocateVirtualMemory_SystemCall_10_0_15063
+	cmp word ptr [rax+120h], 16299
+	je  NtAllocateVirtualMemory_SystemCall_10_0_16299
+	cmp word ptr [rax+120h], 17134
+	je  NtAllocateVirtualMemory_SystemCall_10_0_17134
+	cmp word ptr [rax+120h], 17763
+	je  NtAllocateVirtualMemory_SystemCall_10_0_17763
+	cmp word ptr [rax+120h], 18362
+	je  NtAllocateVirtualMemory_SystemCall_10_0_18362
+	cmp word ptr [rax+120h], 18363
+	je  NtAllocateVirtualMemory_SystemCall_10_0_18363
+	cmp word ptr [rax+120h], 19041
+	je  NtAllocateVirtualMemory_SystemCall_10_0_19041
+	cmp word ptr [rax+120h], 19042
+	je  NtAllocateVirtualMemory_SystemCall_10_0_19042
+	jmp NtAllocateVirtualMemory_SystemCall_Unknown
+NtAllocateVirtualMemory_SystemCall_10_0_10240:        ; Windows 10.0.10240 (1507)
+	mov eax, 0018h
+	jmp NtAllocateVirtualMemory_Epilogue
+NtAllocateVirtualMemory_SystemCall_10_0_10586:        ; Windows 10.0.10586 (1511)
+	mov eax, 0018h
+	jmp NtAllocateVirtualMemory_Epilogue
+NtAllocateVirtualMemory_SystemCall_10_0_14393:        ; Windows 10.0.14393 (1607)
+	mov eax, 0018h
+	jmp NtAllocateVirtualMemory_Epilogue
+NtAllocateVirtualMemory_SystemCall_10_0_15063:        ; Windows 10.0.15063 (1703)
+	mov eax, 0018h
+	jmp NtAllocateVirtualMemory_Epilogue
+NtAllocateVirtualMemory_SystemCall_10_0_16299:        ; Windows 10.0.16299 (1709)
+	mov eax, 0018h
+	jmp NtAllocateVirtualMemory_Epilogue
+NtAllocateVirtualMemory_SystemCall_10_0_17134:        ; Windows 10.0.17134 (1803)
+	mov eax, 0018h
+	jmp NtAllocateVirtualMemory_Epilogue
+NtAllocateVirtualMemory_SystemCall_10_0_17763:        ; Windows 10.0.17763 (1809)
+	mov eax, 0018h
+	jmp NtAllocateVirtualMemory_Epilogue
+NtAllocateVirtualMemory_SystemCall_10_0_18362:        ; Windows 10.0.18362 (1903)
+	mov eax, 0018h
+	jmp NtAllocateVirtualMemory_Epilogue
+NtAllocateVirtualMemory_SystemCall_10_0_18363:        ; Windows 10.0.18363 (1909)
+	mov eax, 0018h
+	jmp NtAllocateVirtualMemory_Epilogue
+NtAllocateVirtualMemory_SystemCall_10_0_19041:        ; Windows 10.0.19041 (2004)
+	mov eax, 0018h
+	jmp NtAllocateVirtualMemory_Epilogue
+NtAllocateVirtualMemory_SystemCall_10_0_19042:        ; Windows 10.0.19042 (20H2)
+	mov eax, 0018h
+	jmp NtAllocateVirtualMemory_Epilogue
+NtAllocateVirtualMemory_SystemCall_Unknown:           ; Unknown/unsupported version.
+	ret
+NtAllocateVirtualMemory_Epilogue:
+	mov r10, rcx
+	syscall
+	ret
+NtAllocateVirtualMemory ENDP
+
+NtWriteVirtualMemory PROC
+	mov rax, gs:[60h]                              ; Load PEB into RAX.
+NtWriteVirtualMemory_Check_X_X_XXXX:               ; Check major version.
+	cmp dword ptr [rax+118h], 10
+	je  NtWriteVirtualMemory_Check_10_0_XXXX
+	jmp NtWriteVirtualMemory_SystemCall_Unknown
+NtWriteVirtualMemory_Check_10_0_XXXX:              ; Check build number for Windows 10.
+	cmp word ptr [rax+120h], 10240
+	je  NtWriteVirtualMemory_SystemCall_10_0_10240
+	cmp word ptr [rax+120h], 10586
+	je  NtWriteVirtualMemory_SystemCall_10_0_10586
+	cmp word ptr [rax+120h], 14393
+	je  NtWriteVirtualMemory_SystemCall_10_0_14393
+	cmp word ptr [rax+120h], 15063
+	je  NtWriteVirtualMemory_SystemCall_10_0_15063
+	cmp word ptr [rax+120h], 16299
+	je  NtWriteVirtualMemory_SystemCall_10_0_16299
+	cmp word ptr [rax+120h], 17134
+	je  NtWriteVirtualMemory_SystemCall_10_0_17134
+	cmp word ptr [rax+120h], 17763
+	je  NtWriteVirtualMemory_SystemCall_10_0_17763
+	cmp word ptr [rax+120h], 18362
+	je  NtWriteVirtualMemory_SystemCall_10_0_18362
+	cmp word ptr [rax+120h], 18363
+	je  NtWriteVirtualMemory_SystemCall_10_0_18363
+	cmp word ptr [rax+120h], 19041
+	je  NtWriteVirtualMemory_SystemCall_10_0_19041
+	cmp word ptr [rax+120h], 19042
+	je  NtWriteVirtualMemory_SystemCall_10_0_19042
+	jmp NtWriteVirtualMemory_SystemCall_Unknown
+NtWriteVirtualMemory_SystemCall_10_0_10240:        ; Windows 10.0.10240 (1507)
+	mov eax, 003ah
+	jmp NtWriteVirtualMemory_Epilogue
+NtWriteVirtualMemory_SystemCall_10_0_10586:        ; Windows 10.0.10586 (1511)
+	mov eax, 003ah
+	jmp NtWriteVirtualMemory_Epilogue
+NtWriteVirtualMemory_SystemCall_10_0_14393:        ; Windows 10.0.14393 (1607)
+	mov eax, 003ah
+	jmp NtWriteVirtualMemory_Epilogue
+NtWriteVirtualMemory_SystemCall_10_0_15063:        ; Windows 10.0.15063 (1703)
+	mov eax, 003ah
+	jmp NtWriteVirtualMemory_Epilogue
+NtWriteVirtualMemory_SystemCall_10_0_16299:        ; Windows 10.0.16299 (1709)
+	mov eax, 003ah
+	jmp NtWriteVirtualMemory_Epilogue
+NtWriteVirtualMemory_SystemCall_10_0_17134:        ; Windows 10.0.17134 (1803)
+	mov eax, 003ah
+	jmp NtWriteVirtualMemory_Epilogue
+NtWriteVirtualMemory_SystemCall_10_0_17763:        ; Windows 10.0.17763 (1809)
+	mov eax, 003ah
+	jmp NtWriteVirtualMemory_Epilogue
+NtWriteVirtualMemory_SystemCall_10_0_18362:        ; Windows 10.0.18362 (1903)
+	mov eax, 003ah
+	jmp NtWriteVirtualMemory_Epilogue
+NtWriteVirtualMemory_SystemCall_10_0_18363:        ; Windows 10.0.18363 (1909)
+	mov eax, 003ah
+	jmp NtWriteVirtualMemory_Epilogue
+NtWriteVirtualMemory_SystemCall_10_0_19041:        ; Windows 10.0.19041 (2004)
+	mov eax, 003ah
+	jmp NtWriteVirtualMemory_Epilogue
+NtWriteVirtualMemory_SystemCall_10_0_19042:        ; Windows 10.0.19042 (20H2)
+	mov eax, 003ah
+	jmp NtWriteVirtualMemory_Epilogue
+NtWriteVirtualMemory_SystemCall_Unknown:           ; Unknown/unsupported version.
+	ret
+NtWriteVirtualMemory_Epilogue:
+	mov r10, rcx
+	syscall
+	ret
+NtWriteVirtualMemory ENDP
+
+NtCreateThreadEx PROC
+	mov rax, gs:[60h]                          ; Load PEB into RAX.
+NtCreateThreadEx_Check_X_X_XXXX:               ; Check major version.
+	cmp dword ptr [rax+118h], 10
+	je  NtCreateThreadEx_Check_10_0_XXXX
+	jmp NtCreateThreadEx_SystemCall_Unknown
+NtCreateThreadEx_Check_10_0_XXXX:              ; Check build number for Windows 10.
+	cmp word ptr [rax+120h], 10240
+	je  NtCreateThreadEx_SystemCall_10_0_10240
+	cmp word ptr [rax+120h], 10586
+	je  NtCreateThreadEx_SystemCall_10_0_10586
+	cmp word ptr [rax+120h], 14393
+	je  NtCreateThreadEx_SystemCall_10_0_14393
+	cmp word ptr [rax+120h], 15063
+	je  NtCreateThreadEx_SystemCall_10_0_15063
+	cmp word ptr [rax+120h], 16299
+	je  NtCreateThreadEx_SystemCall_10_0_16299
+	cmp word ptr [rax+120h], 17134
+	je  NtCreateThreadEx_SystemCall_10_0_17134
+	cmp word ptr [rax+120h], 17763
+	je  NtCreateThreadEx_SystemCall_10_0_17763
+	cmp word ptr [rax+120h], 18362
+	je  NtCreateThreadEx_SystemCall_10_0_18362
+	cmp word ptr [rax+120h], 18363
+	je  NtCreateThreadEx_SystemCall_10_0_18363
+	cmp word ptr [rax+120h], 19041
+	je  NtCreateThreadEx_SystemCall_10_0_19041
+	cmp word ptr [rax+120h], 19042
+	je  NtCreateThreadEx_SystemCall_10_0_19042
+	jmp NtCreateThreadEx_SystemCall_Unknown
+NtCreateThreadEx_SystemCall_10_0_10240:        ; Windows 10.0.10240 (1507)
+	mov eax, 00b3h
+	jmp NtCreateThreadEx_Epilogue
+NtCreateThreadEx_SystemCall_10_0_10586:        ; Windows 10.0.10586 (1511)
+	mov eax, 00b4h
+	jmp NtCreateThreadEx_Epilogue
+NtCreateThreadEx_SystemCall_10_0_14393:        ; Windows 10.0.14393 (1607)
+	mov eax, 00b6h
+	jmp NtCreateThreadEx_Epilogue
+NtCreateThreadEx_SystemCall_10_0_15063:        ; Windows 10.0.15063 (1703)
+	mov eax, 00b9h
+	jmp NtCreateThreadEx_Epilogue
+NtCreateThreadEx_SystemCall_10_0_16299:        ; Windows 10.0.16299 (1709)
+	mov eax, 00bah
+	jmp NtCreateThreadEx_Epilogue
+NtCreateThreadEx_SystemCall_10_0_17134:        ; Windows 10.0.17134 (1803)
+	mov eax, 00bbh
+	jmp NtCreateThreadEx_Epilogue
+NtCreateThreadEx_SystemCall_10_0_17763:        ; Windows 10.0.17763 (1809)
+	mov eax, 00bch
+	jmp NtCreateThreadEx_Epilogue
+NtCreateThreadEx_SystemCall_10_0_18362:        ; Windows 10.0.18362 (1903)
+	mov eax, 00bdh
+	jmp NtCreateThreadEx_Epilogue
+NtCreateThreadEx_SystemCall_10_0_18363:        ; Windows 10.0.18363 (1909)
+	mov eax, 00bdh
+	jmp NtCreateThreadEx_Epilogue
+NtCreateThreadEx_SystemCall_10_0_19041:        ; Windows 10.0.19041 (2004)
+	mov eax, 00c1h
+	jmp NtCreateThreadEx_Epilogue
+NtCreateThreadEx_SystemCall_10_0_19042:        ; Windows 10.0.19042 (20H2)
+	mov eax, 00c1h
+	jmp NtCreateThreadEx_Epilogue
+NtCreateThreadEx_SystemCall_Unknown:           ; Unknown/unsupported version.
+	ret
+NtCreateThreadEx_Epilogue:
+	mov r10, rcx
+	syscall
+	ret
+NtCreateThreadEx ENDP
+
+end
